1a3ccb0084e792cb38d19c7ce4e73599f7874ff4
Deniz Sağlam <reincore@users.noreply.github.com>
2023-12-11 23:48:56 +0300

    Switch API from API-Football to SportsDataIO due to faulty API responses

--- mac_bildirimi.py
+++ mac_bildirimi.py
@@ -1,6 +1,5 @@
 import requests
 from datetime import datetime
-import json
 import os
 
 def trigger_ifttt_webhook(event, value1, value2, value3):
@@ -20,37 +19,39 @@ def trigger_ifttt_webhook(event, value1, value2, value3):
 
     return response.status_code == 200
 
-def check_game(venue_id, date):
-    url = f"https://api-football-v1.p.rapidapi.com/v3/fixtures?venue={venue_id}&date={date}"
+def check_games_for_team(fixtures, team_id):
+    for fixture in fixtures:
+        if fixture['HomeTeamId'] == team_id:
+            home_team = fixture['HomeTeamName']
+            away_team = fixture['AwayTeamName']
+            venue_name = fixture['VenueName']
 
+            if trigger_ifttt_webhook("mac_bildirimi", home_team, away_team, venue_name):
+                print(f"IFTTT Webhook triggered successfully for the game between {home_team} and {away_team} at {venue_name}!")
+            else:
+                print("Failed to trigger IFTTT Webhook.")
+            return
+    print(f"No home game today for team ID {team_id}.")
+
+def get_todays_games():
+    date = datetime.now().strftime('%Y-%m-%d')
+    url = f"https://api.sportsdata.io/v3/soccer/scores/json/GamesByDate/{date}"
     headers = {
-        "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
-        "x-rapidapi-key": os.environ['RAPID_API_KEY']
+        "key": os.environ['SPORTSDATA_IO_API_KEY']
     }
 
-    response = requests.request("GET", url, headers=headers)
+    response = requests.get(url, headers=headers)
+    
+    # Log the fixture data
+    print(f"Fixture obtained: {response.json()}")
+    
+    return response.json()
 
-    fixtures = response.json()
-    print("Fixtures: ", fixtures)
+# Main execution
+fixtures = get_todays_games()
 
-    if fixtures['results']:
-        fixture = fixtures['response'][0]
-        home_team = fixture['teams']['home']['name']
-        away_team = fixture['teams']['away']['name']
-        venue_name = fixture['fixture']['venue']['name']
+fenerbahce_id = 572  # SportsData.io ID for Fenerbahçe
+besiktas_id = 728    # SportsData.io ID for Beşiktaş
 
-
-        if trigger_ifttt_webhook("mac_bildirimi", home_team, away_team, venue_name):
-            print(f"IFTTT Webhook triggered successfully for the game between {home_team} and {away_team} at {venue_name}!")
-        else:
-            print("Failed to trigger IFTTT Webhook.")
-    else:
-        print("No game today.")
-
-fenerbahce_venue_id = 1581
-besiktas_venue_id = 20301
-date = datetime.now().strftime('%Y-%m-%d')
-
-
-check_game(fenerbahce_venue_id, date)
-check_game(besiktas_venue_id, date)
+check_games_for_team(fixtures, fenerbahce_id)
+check_games_for_team(fixtures, besiktas_id)
